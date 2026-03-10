a1afc3667faee433bde33f4e3fd4b629cb6dbde8
Arif Raza <arif3raza@gmail.com>
2026-03-10 22:15:53 +0530

    Update EC2_instance.tf

--- ec2_instance_creation/EC2_instance.tf
+++ ec2_instance_creation/EC2_instance.tf
@@ -7,3 +7,23 @@ resource "aws_key_pair" "mykey" {
   key_name   = "terraform-key"
   public_key = file("id_rsa.pub")
 }
+#vpc
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+#subnet
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+}
+#Security-Group
+resource "aws_security_group" "mySG" {
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
