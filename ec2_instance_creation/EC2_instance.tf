63d814b925bc3a07680d8333e1ce60ca205ee30d
Arif Raza <arif3raza@gmail.com>
2026-03-10 22:19:26 +0530

    Update EC2_instance.tf

--- ec2_instance_creation/EC2_instance.tf
+++ ec2_instance_creation/EC2_instance.tf
@@ -27,3 +27,10 @@ resource "aws_security_group" "mySG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+  engress{
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
