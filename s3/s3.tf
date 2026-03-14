12681d9c6690bd9ad3398c99d57276af478f8e32
Arif Raza <arif3raza@gmail.com>
2026-03-14 22:43:44 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -10,3 +10,15 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "arif-devops-terraform-bucket-12345"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
