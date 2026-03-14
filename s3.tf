7a2079938db940043329c1dd6678e1f78d967d2d
Arif Raza <arif3raza@gmail.com>
2026-03-14 22:42:25 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "arif-devops-terraform-bucket-12345"
+
+  tags = {
+    Name        = "ArifTerraformBucket"
+    Environment = "Dev"
+  }
+}
