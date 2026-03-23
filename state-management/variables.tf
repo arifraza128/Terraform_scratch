6d5deb6a2685a26c1f3c19a10d79e0b7ae0b1dec
Arif Raza <arif3raza@gmail.com>
2026-03-23 22:38:32 +0530

    Add Terraform variables for AWS configuration
    Define variables for AWS region, AMI ID, and instance type.

---
+++ state-management/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  default     = "ami-0f5ee92e2d63afc18"  # Amazon Linux (Mumbai)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
