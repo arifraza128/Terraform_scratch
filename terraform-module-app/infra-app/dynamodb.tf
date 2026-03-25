3bbbc2e0fc3fbd02d10b1b21adb8dba4536500af
Arif Raza <arif3raza@gmail.com>
2026-03-25 21:29:27 +0530

    Create dynamodb.tf

---
+++ terraform-module-app/infra-app/dynamodb.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "app_table" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "${var.env}-dynamodb"
+    Environment = var.env
+  }
+}
