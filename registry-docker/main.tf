40791e913b5c00525518c55c212f10546997af3f
Arif Raza <arif3raza@gmail.com>
2026-04-01 22:42:06 +0530

    Add AWS ECR repository configuration

--- registry-docker/main.tf
+++ registry-docker/main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_ecr_repository" "my_repo" {
+  name                 = var.repo_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "Arif ECR Repo"
+    Environment = "Dev"
+  }
+}
