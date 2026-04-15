40a4a08bd302d21db0123ef50198e4725a29b728
Arif Raza <arif3raza@gmail.com>
2026-04-15 20:39:00 +0530

    Add VPC and EKS cluster configuration

--- eks-cluster-terraform/main.tf
+++ eks-cluster-terraform/main.tf
@@ -1,1 +1,55 @@
+# VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0"
 
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+
+# EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# Node Group
+module "eks_node_group" {
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  version = "20.0"
+
+  cluster_name    = module.eks.cluster_name
+  cluster_version = module.eks.cluster_version
+
+  name       = "node-group"
+  subnet_ids = module.vpc.private_subnets
+
+  instance_types = [var.node_instance_type]
+
+  desired_size = 2
+  max_size     = 3
+  min_size     = 1
+}
