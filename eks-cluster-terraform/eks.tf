f9149d1127bfa86602e436c03aa75c251327af6e
Arif Raza <arif3raza@gmail.com>
2026-04-18 19:03:52 +0530

    Update eks.tf

--- eks-cluster-terraform/eks.tf
+++ eks-cluster-terraform/eks.tf
@@ -1,1 +1,16 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0"
 
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  tags = {
+    Environment = "dev"
+  }
+}
